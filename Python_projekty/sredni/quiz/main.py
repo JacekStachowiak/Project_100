4f7a8777724dc3093bce2114320208b65df5105d
Jacek Stachowiak <jacek.stachowiak@onet.com.pl>
2023-01-17 14:29:17 +0100

    new project - quiz

---
+++ Python_projekty/sredni/quiz/main.py
@@ -0,0 +1,24 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for x in question_data:
+    x_text = x['text']
+    x_answer = x['answer']
+    new_question = Question(x_text, x_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+
+while quiz.still_has_question():    # is True
+    quiz.next_question()
+
+print('You have completed the quiz')
+print(f'Your final score was: {quiz.score}/{quiz.question_number}')
+
+
+
+
