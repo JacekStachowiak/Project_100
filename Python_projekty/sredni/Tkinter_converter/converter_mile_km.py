c2deb153a09c4228803bf93e5c638400c03492de
Jacek Stachowiak <jacek.stachowiak@onet.com.pl>
2023-01-23 19:21:35 +0100

    new project - dokończenie kodu

--- Python_projekty/sredni/Tkinter_converter/converter_mile_km.py
+++ Python_projekty/sredni/Tkinter_converter/converter_mile_km.py
@@ -1,12 +1,40 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
-window.minsize(width=350, height=150)
+
+def click_calculate():
+    miles = float(miles_input.get())
+    km = miles * 1.609
+    kilometers = round(km, 2)
+    km_result_label.config(text=f'{kilometers}')
+
+
+window = Tk()
+window.minsize(width=250, height=125)
 window.title('Konwersja')
+window.config(padx=20, pady=20)
 
-etykieta = tkinter.Label(text='Miles', font=('Arial', 14, 'bold'))
-etykieta.grid(column=4, row=1)
+miles_input = Entry(width=15)
+miles_input.grid(column=1, row=0)
+miles_input.get()
 
+miles_label = Label(text='Miles')
+miles_label.grid(column=2, row=0)
+miles_label.config(padx=5, pady=5)
 
+is_equal_label = Label(text='is equal')
+is_equal_label.grid(column=0, row=1)
+is_equal_label.config(padx=5, pady=5)
+
+km_result_label = Label(text=0)
+km_result_label.grid(column=1, row=1)
+km_result_label.config(padx=5, pady=5)
+
+km_label = Label(text='km')
+km_label.grid(column=2, row=1)
+km_label.config(padx=5, pady=5)
+
+calculate_button = Button(text='Calculate', command=click_calculate)
+calculate_button.grid(column=1, row=2)
+calculate_button.config(padx=5, pady=5)
 
 window.mainloop()
